29bc80fd0256a95d815085481a0beaf467fbbbb0
briansukalski <sukalskibrian@gmail.com>
2022-02-08 19:13:48 -0600

    Added scrollprint file to directory to use to create an easier-to-follow reading experience for the user

---
+++ scrollprint.py
@@ -0,0 +1,19 @@
+import random
+import time
+import sys
+
+#Delays time between characters while printing to allow messages to be more easily read while printing
+def scroll_print(msg):
+    for i in msg:
+        if i == "\n":
+            time.sleep(1)
+        elif i == "_":
+            time.sleep(0.2)
+        else:
+            time.sleep(0.01)
+        sys.stdout.write(i)
+        sys.stdout.flush()
+
+def scroll_input(msg):
+    scroll_print(msg)
+    return input("\n")
